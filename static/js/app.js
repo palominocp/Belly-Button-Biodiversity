264b6591c249887fb2e9fa4ab2f59f7a4e5bb83d
Carlos Palomino <palominocp@outlook.com>
2020-10-06 08:15:39 -0500

    Top 10 OTUs review

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,8 @@
 d3.json("samples.json").then((data) => {
   //  Create the Traces
+  console.log(data.names);
+  var indiv = 953;
+  console.log(data.names.indexOf(indiv.toString()));
   console.log(data.samples[0].otu_ids.slice(0,10));
   console.log(data.samples[0].sample_values.slice(0,10));
   var trace1 = {
