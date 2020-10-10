c1387515e447b7f959197b5aa0d2e9cdc0cae023
Carlos Palomino <palominocp@outlook.com>
2020-10-10 11:12:59 -0500

    Code reviewed and comments added

--- static/js/app.js
+++ static/js/app.js
@@ -1,123 +1,49 @@
 d3.json("samples.json").then((data) => {
-  var newSelect = document.getElementById("selDataset");
-  for (index = 0; index < data.names.length; index++) {
-    var opt = document.createElement("option");
-    opt.text = data.names[index];
-    //opt.innerHTML = element; // whatever property it has
 
-    // then append it to the select element
-    newSelect.appendChild(opt);
-  };
-
-  var indx = 0;
-  var yvalues = data.samples[indx].otu_ids.slice(0,10);
-    var yvaluesf = yvalues.map(item => {
-      return "OTU " + item.toString();
-    });
-  var trace1 = {
-    y: yvaluesf.reverse(),
-    x: data.samples[indx].sample_values.slice(0,10).reverse(),
-    type: "bar",
-    orientation: 'h'
-  };
-
-var data_bar = [trace1];
-
-    // Define the plot layout
-    var layout = {
-      title: "Top 10 OTUs",
-      xaxis: { title: "otu_ids" },
-      yaxis: { title: "sample_values" }
+    // Populate the DOM select element
+    var select = document.getElementById("selDataset");
+    for (i = 0; i < data.names.length; i++) {
+        var option = document.createElement("option");
+        option.text = data.names[i];
+        select.appendChild(option);
     };
 
-    // Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("bar", data_bar, layout);
-    
-    var trace2 = {
-      x: data.samples[indx].otu_ids,
-      y: data.samples[indx].sample_values,
-      mode: 'markers',
-      marker: {
-        size: data.samples[indx].sample_values,
-        color: data.samples[indx].otu_ids
-      }
-    };
-
-    var data_bubble = [trace2];
-
-    var layout = {
-        title: 'Bubble chart',
-    }
-
-    Plotly.newPlot('bubble', data_bubble, layout);
-
-    d3.selectAll('h6').remove();
-    d3.select(".panel-body").insert("h6").text("id: " + data.metadata[indx].id);
-    d3.select(".panel-body").insert("h6").text("ethnicity: " + data.metadata[indx].ethnicity);
-    d3.select(".panel-body").insert("h6").text("gender: " + data.metadata[indx].gender);
-    d3.select(".panel-body").insert("h6").text("age: " + data.metadata[indx].age);
-    d3.select(".panel-body").insert("h6").text("location: " + data.metadata[indx].location);
-    d3.select(".panel-body").insert("h6").text("bbtype: " + data.metadata[indx].bbtype);
-    d3.select(".panel-body").insert("h6").text("wfreq: " + data.metadata[indx].wfreq);
-
-});
-
-
-
-
-// then append the select to an element in the dom
-d3.selectAll("#selDataset").on("change", updatePlotly);
-  
-function updatePlotly() {
-  var dropdownMenu = d3.select("#selDataset");
-  var dataset = dropdownMenu.property("value");
-
-  d3.json("samples.json").then((data) => {
-
-    //  Create the Traces
-    //console.log(data.names);
-    var indiv = dataset;
-    var indx = data.names.indexOf(indiv.toString());
-    console.log(data.samples[indx].otu_ids.slice(0,10));
-    console.log(data.samples[indx].sample_values.slice(0,10));
-    console.log(data.samples[indx].otu_labels.slice(0,10));
-    var yvalues = data.samples[indx].otu_ids.slice(0,10);
-    var yvaluesf = yvalues.map(item => {
-      return "OTU " + item.toString();
+    // Show sample for first individual (index = 0)
+    var index = 0;
+    var topOtuids = data.samples[index].otu_ids.slice(0,10);
+    var yvalues = topOtuids.map(item => {
+        return "OTU " + item.toString();
     });
+
+    // Bar chart
     var trace1 = {
-      y: yvaluesf.reverse(),
-      x: data.samples[indx].sample_values.slice(0,10).reverse(),
-      type: "bar",
-      orientation: 'h'
+        y: yvalues.reverse(),
+        x: data.samples[index].sample_values.slice(0,10).reverse(),
+        type: "bar",
+        orientation: 'h'
     };
 
-    console.log(trace1);
-
-    // Create the data array for the plot
     var data_bar = [trace1];
 
     // Define the plot layout
     var layout = {
-      title: "Top 10 OTUs",
-      xaxis: { title: "otu_ids" },
-      yaxis: { title: "sample_values" }
+        title: "Top 10 OTUs",
+        xaxis: { title: "otu_ids" },
+        yaxis: { title: "sample_values" }
     };
 
-    // Plot the chart to a div tag with id "plot"
+    // Plot the chart to "bar" div
     Plotly.newPlot("bar", data_bar, layout);
     
-    console.log(indiv);
-    console.log(data.names.indexOf(indiv.toString())); // indx
-
+    // Bubble chart
     var trace2 = {
-      x: data.samples[indx].otu_ids,
-      y: data.samples[indx].sample_values,
-      mode: 'markers',
-      marker: {
-        size: data.samples[indx].sample_values,
-        color: data.samples[indx].otu_ids
-      }
+        x: data.samples[index].otu_ids,
+        y: data.samples[index].sample_values,
+        mode: 'markers',
+        marker: {
+            size: data.samples[index].sample_values,
+            color: data.samples[index].otu_ids
+        }
     };
 
     var data_bubble = [trace2];
@@ -126,15 +52,86 @@ function updatePlotly() {
         title: 'Bubble chart',
     }
 
+    // Plot the chart to "bubble" div
     Plotly.newPlot('bubble', data_bubble, layout);
 
     d3.selectAll('h6').remove();
-    d3.select(".panel-body").insert("h6").text("id: " + data.metadata[indx].id);
-    d3.select(".panel-body").insert("h6").text("ethnicity: " + data.metadata[indx].ethnicity);
-    d3.select(".panel-body").insert("h6").text("gender: " + data.metadata[indx].gender);
-    d3.select(".panel-body").insert("h6").text("age: " + data.metadata[indx].age);
-    d3.select(".panel-body").insert("h6").text("location: " + data.metadata[indx].location);
-    d3.select(".panel-body").insert("h6").text("bbtype: " + data.metadata[indx].bbtype);
-    d3.select(".panel-body").insert("h6").text("wfreq: " + data.metadata[indx].wfreq);
+    d3.select(".panel-body").insert("h6").text("id: " + data.metadata[index].id);
+    d3.select(".panel-body").insert("h6").text("ethnicity: " + data.metadata[index].ethnicity);
+    d3.select(".panel-body").insert("h6").text("gender: " + data.metadata[index].gender);
+    d3.select(".panel-body").insert("h6").text("age: " + data.metadata[index].age);
+    d3.select(".panel-body").insert("h6").text("location: " + data.metadata[index].location);
+    d3.select(".panel-body").insert("h6").text("bbtype: " + data.metadata[index].bbtype);
+    d3.select(".panel-body").insert("h6").text("wfreq: " + data.metadata[index].wfreq);
+
+});
+
+// Run updatePlotly function whenever there is a new selection
+d3.selectAll("#selDataset").on("change", updatePlotly);
+  
+function updatePlotly() {
+
+    // Get the selected value
+    var dropdownMenu = d3.select("#selDataset");
+    var dataset = dropdownMenu.property("value");
+
+    d3.json("samples.json").then((data) => {
+
+        // Get the index of selected sample
+        var index = data.names.indexOf(dataset.toString());
+        var topOtuids = data.samples[index].otu_ids.slice(0,10);
+        var yvalues = topOtuids.map(item => {
+            return "OTU " + item.toString();
+        });
+        
+        // Bar chart
+        var trace1 = {
+            y: yvalues.reverse(),
+            x: data.samples[index].sample_values.slice(0,10).reverse(),
+            type: "bar",
+            orientation: 'h'
+        };
+
+        // Create the data array for the plot
+        var data_bar = [trace1];
+
+        // Define the plot layout
+        var layout = {
+            title: "Top 10 OTUs",
+            xaxis: { title: "otu_ids" },
+            yaxis: { title: "sample_values" }
+        };
+
+        // Plot the chart to "bar" div
+        Plotly.newPlot("bar", data_bar, layout);
+    
+        // Bubble chart
+        var trace2 = {
+            x: data.samples[index].otu_ids,
+            y: data.samples[index].sample_values,
+            mode: 'markers',
+            marker: {
+                size: data.samples[index].sample_values,
+                color: data.samples[index].otu_ids
+            }
+        };
+
+        var data_bubble = [trace2];
+
+        var layout = {
+            title: 'Bubble chart',
+        }
+
+        // Plot the chart to "bubble" div
+        Plotly.newPlot('bubble', data_bubble, layout);
+
+    d3.selectAll('h6').remove();
+    d3.select(".panel-body").insert("h6").text("id: " + data.metadata[index].id);
+    d3.select(".panel-body").insert("h6").text("ethnicity: " + data.metadata[index].ethnicity);
+    d3.select(".panel-body").insert("h6").text("gender: " + data.metadata[index].gender);
+    d3.select(".panel-body").insert("h6").text("age: " + data.metadata[index].age);
+    d3.select(".panel-body").insert("h6").text("location: " + data.metadata[index].location);
+    d3.select(".panel-body").insert("h6").text("bbtype: " + data.metadata[index].bbtype);
+    d3.select(".panel-body").insert("h6").text("wfreq: " + data.metadata[index].wfreq);
   });
 };
