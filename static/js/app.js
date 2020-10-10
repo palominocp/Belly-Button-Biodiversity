4b9c9c644630177537d7617329df1e45f7992e56
Carlos Palomino <palominocp@outlook.com>
2020-10-10 07:50:55 -0500

    Bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -23,25 +23,24 @@ function updatePlotly() {
     //console.log(data.names);
     var indiv = dataset;
     var indx = data.names.indexOf(indiv.toString());
-    console.log(indiv);
-    console.log(indx);
-      console.log(data.samples[indx].otu_ids.slice(0,10));
-      console.log(data.samples[indx].sample_values.slice(0,10));
-      var yvalues = data.samples[indx].otu_ids.slice(0,10);
-      var yvaluesf = yvalues.map(item => {
-        return "OTU " + item.toString();
-      });
-      var trace1 = {
-        y: yvaluesf.reverse(),
-        x: data.samples[indx].sample_values.slice(0,10).reverse(),
-        type: "bar",
-        orientation: 'h'
-      };
+    console.log(data.samples[indx].otu_ids.slice(0,10));
+    console.log(data.samples[indx].sample_values.slice(0,10));
+    console.log(data.samples[indx].otu_labels.slice(0,10));
+    var yvalues = data.samples[indx].otu_ids.slice(0,10);
+    var yvaluesf = yvalues.map(item => {
+      return "OTU " + item.toString();
+    });
+    var trace1 = {
+      y: yvaluesf.reverse(),
+      x: data.samples[indx].sample_values.slice(0,10).reverse(),
+      type: "bar",
+      orientation: 'h'
+    };
 
     console.log(trace1);
 
     // Create the data array for the plot
-    var data = [trace1];
+    var data_bar = [trace1];
 
     // Define the plot layout
     var layout = {
@@ -51,6 +50,27 @@ function updatePlotly() {
     };
 
     // Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("plot", data, layout);
+    Plotly.newPlot("bar", data_bar, layout);
+    
+    console.log(indiv);
+    console.log(data.names.indexOf(indiv.toString())); // indx
+
+    var trace2 = {
+      x: data.samples[indx].otu_ids,
+      y: data.samples[indx].sample_values,
+      mode: 'markers',
+      marker: {
+        size: data.samples[indx].sample_values
+        // color: data.samples[indx].otu_ids
+      }
+    };
+
+    var data_bubble = [trace2];
+
+    var layout = {
+        title: 'Bubble chart',
+    }
+
+    Plotly.newPlot('bubble', data_bubble, layout);
   });
 };
